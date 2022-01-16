912325685580102635efac000fc68b24d13e39d5
anuj.katoch <anuj.katoch@timesinternet.in>
2022-01-17 01:26:32 +0530

    read operation

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,12 +2,12 @@ const mongodb = require ("mongodb");
 
  const {MongoClient,ObjectId} = mongodb;
 
-const id =  new ObjectId();
-console.log(id);
-console.log(id.getTimestamp());
-console.log(id.id); // buffer
-console.log(id.id.length); //lenth 12
-console.log(id.toHexString().length); //length 24
+// const id =  new ObjectId();
+// console.log(id);
+// console.log(id.getTimestamp());
+// console.log(id.id); // buffer
+// console.log(id.id.length); //lenth 12
+// console.log(id.toHexString().length); //length 24
 
 const databaseName="task-manager";
 const connectionURL = "mongodb://127.0.0.1:27017";
@@ -16,7 +16,8 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(err,client)=>{
     if(err){
         return console.log("some error in connection")
     }
-    // const db=client.db(databaseName);
+    //Writing a doc
+    const db=client.db(databaseName);
     // db.collection('user').insertOne({
     //     _id:id, //adding manual id
     //     name:'rohn',
@@ -44,4 +45,18 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(err,client)=>{
     //         return console.log("data not added");
     //     } console.log(result.insertedCount);
     // })
+
+    // Reading a doc
+    // db.collection('task').findOne({_id:new ObjectId("61e3c8c854a0cf6660d4913f")},(error,user)=>{
+    //     if(error){
+    //         return console.log("user not found")
+    //     }
+    //     console.log(user);
+    // })
+    db.collection('task').find({'description':'project 1'}).toArray((error,user)=>{
+        if(error){
+            return console.log("user not found")
+        }
+        console.log(user);
+    })
 })
