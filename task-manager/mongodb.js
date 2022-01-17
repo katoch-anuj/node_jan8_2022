33b53025ddc03693c11fe5009a70f3e99d53d382
anuj.katoch <anuj.katoch@timesinternet.in>
2022-01-17 19:06:14 +0530

    CRUD operation

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,6 +1,6 @@
-const mongodb = require ("mongodb");
+const mongodb = require("mongodb");
 
- const {MongoClient,ObjectId} = mongodb;
+const { MongoClient, ObjectId } = mongodb;
 
 // const id =  new ObjectId();
 // console.log(id);
@@ -9,54 +9,75 @@ const mongodb = require ("mongodb");
 // console.log(id.id.length); //lenth 12
 // console.log(id.toHexString().length); //length 24
 
-const databaseName="task-manager";
+const databaseName = "task-manager";
 const connectionURL = "mongodb://127.0.0.1:27017";
 
-MongoClient.connect(connectionURL,{useNewUrlParser:true},(err,client)=>{
-    if(err){
-        return console.log("some error in connection")
-    }
-    //Writing a doc
-    const db=client.db(databaseName);
-    // db.collection('user').insertOne({
-    //     _id:id, //adding manual id
-    //     name:'rohn',
-    //     age:21
-    // },(error,result)=>{
-    //     if(error){
-    //         return console.log("unable to update the db")
-    //     }
-    //     console.log(result.acknowledged);
-    // })
-    // db.collection('task').insertMany([
-    //     {
-    //         description:"project 1",
-    //         completed: true
-    //     },
-    //     {
-    //         description:"project 2",
-    //         completed: false
-    //     },{
-    //         description:"project 3",
-    //         completed: true
-    //     }
-    // ],(error, result)=>{
-    //     if (error){
-    //         return console.log("data not added");
-    //     } console.log(result.insertedCount);
-    // })
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log("some error in connection");
+  }
+  //Writing a doc
+  const db=client.db(databaseName);
+  // db.collection('user').insertOne({
+  //     // _id:id, //adding manual id
+  //     name:'rahul',
+  //     age:21
+  // },(error,result)=>{
+  //     if(error){
+  //         return console.log("unable to update the db")
+  //     }
+  //     console.log(result.acknowledged);
+  // })
+  // db.collection('task').insertMany([
+  //     {
+  //         description:"project 1",
+  //         completed: true
+  //     },
+  //     {
+  //         description:"project 2",
+  //         completed: false
+  //     },{
+  //         description:"project 3",
+  //         completed: true
+  //     }
+  // ],(error, result)=>{
+  //     if (error){
+  //         return console.log("data not added");
+  //     } console.log(result.insertedCount);
+  // })
 
-    // Reading a doc
-    // db.collection('task').findOne({_id:new ObjectId("61e3c8c854a0cf6660d4913f")},(error,user)=>{
-    //     if(error){
-    //         return console.log("user not found")
-    //     }
-    //     console.log(user);
-    // })
-    db.collection('task').find({'description':'project 1'}).toArray((error,user)=>{
-        if(error){
-            return console.log("user not found")
-        }
-        console.log(user);
-    })
-})
+  // Reading a doc
+  // db.collection('task').findOne({_id:new ObjectId("61e3c8c854a0cf6660d4913f")},(error,user)=>{
+  //     if(error){
+  //         return console.log("user not found")
+  //     }
+  //     console.log(user);
+  // })
+  // db.collection('task').find({'description':'project 1'}).toArray((error,user)=>{
+  //     if(error){
+  //         return console.log("user not found")
+  //     }
+  //     console.log(user);
+  // })
+
+  //Updating a doc
+//   db.collection("user").updateOne({ 'name': "rahul kumar" }, { $inc: {'age':1} }).then(result=>{
+//       console.log(result)
+//   }).catch(error=>{
+//       console.log('error');
+//   });
+// db.collection("user").updateMany({ 'name': "rohan" }, { $set: {'name':'rohan kumar'} }).then(result=>{
+//     console.log(result)
+// }).catch(error=>{
+//     console.log('error');
+// });
+
+//Delete
+  db.collection('user').deleteMany({
+      "name":"rahul kumar"
+  }).then(result=>{
+    console.log(result)
+  }).catch(error=>{
+      console.log("error")
+  })
+});
