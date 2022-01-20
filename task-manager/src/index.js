5c89d31738d031c6445ecc4c5da07f37aa7cc7f5
anuj.katoch <anuj.katoch@timesinternet.in>
2022-01-20 23:49:32 +0530

    CRUD operation in mogoose using async and await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,61 +8,176 @@ const port = process.env.PORT || 4000;
 //this ensures the parsing of json into object which can be easily used in req
 app.use(express.json());
 
-app.post('/user',(req,res)=>{
+/**
+ *  writing
+ */
+app.post('/user',async (req,res)=>{
     var user = new User(req.body);
-    user.save().then((result)=>{
-        res.send(result)
-    }).catch((error)=>{
-        res.status(400).send(error);
-    })
-})
-app.post('/task',(req,res)=>{
-    const task=new Task(req.body);
-    task.save().then(()=>{
-        res.send(task)
-    }).catch(error=>{
-        res.status(400).send(error);
-    })
+    try{
+        const result=await user.save()
+        res.send(result);
+    }catch(e){
+        res.status(400).send(e);
+    }
 })
 
-app.get("/users",(req,res)=>{
-    User.find({}).then(result=>{
-        res.send(result);
-    }).catch(error=>{
+
+/**
+ *  Reading
+ */
+app.get("/users", async (req,res)=>{
+    try{
+        const user=await User.find({});
+        res.send(user);
+    }catch(e){
         res.status(500).send()
-    })
+    }
 })
-app.get("/users/:id",(req,res)=>{
+app.get("/users/:id", async (req,res)=>{
     const _id=req.params.id;
-    User.findById(_id).then(user=>{
-        //if we dont get nay result, for mongo db that's success cz mongo db was able to search what u asked and came up with no result.
+    try{
+        const user=await User.findById(_id);
         if(!user){
             res.status(404).send("not found")
         }
         res.send(user);
-    }).catch(error=>{
+    }
+    catch(e){
         res.status(500).send()
-    })
+    }
+    // User.findById(_id).then(user=>{
+    //     //if we dont get nay result, for mongo db that's success cz mongo db was able to search what u asked and came up with no result.
+    //     if(!user){
+    //         res.status(404).send("not found")
+    //     }
+    //     res.send(user);
+    // }).catch(error=>{
+    //     res.status(500).send()
+    // })
 })
+/**
+ *  Updating
+ */
 
-app.get("/tasks",(req,res)=>{
-    Task.find({}).then(task=>{
-        res.send(task)
-    }).catch(error=>{
-        res.status(400).send(error)
-    })
-})
+app.patch('/users/:id',async (req,res)=>{
+    const id=req.params.id;
+    const updates=Object.keys(req.body);
+    const allowedupdates=['name','age','email','password']
+    const isValid = updates.every(update=>allowedupdates.includes(update));
 
-app.get('/tasks/:id',(req,res)=>{
-    const _id=req.params.id
-    Task.findById(_id).then(task=>{
-        if(!task){
-            res.status(400).send("not found")
+    if(!isValid){
+        return res.status(400).send("not valid operation") 
+    }
+    try{
+        //new ensures that updated result is returned, runvlaidators ensure that validation occurs else in this case empty name will be saved
+        const user= await User.findByIdAndUpdate(id,req.body,{new:true,runValidators:true});
+        if(!user){
+            res.status(400).send()
         }
+        res.send(user)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+})
+app.delete('/users/:id',async(req,res)=>{
+    try{
+        const user= await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            res.status(400).send("not found user")
+        }
+        res.send(user);
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+/***
+ * ------------Task section------------
+ */
+ app.post('/task',async (req,res)=>{
+    const task=new Task(req.body);
+    try{
+        await task.save();
         res.send(task)
-    }).catch(error=>{
-        res.status(400).send(error)
-    })
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+    // task.save().then(()=>{
+    //     res.send(task)
+    // }).catch(error=>{
+    //     res.status(400).send(error);
+    // })
+})
+
+app.get("/tasks", async (req,res)=>{
+    try{
+        const task=await Task.find({});
+        res.send(task);
+    }catch(e){
+        res.status(500).send()
+    }
+    // Task.find({}).then(task=>{
+    //     res.send(task)
+    // }).catch(error=>{
+    //     res.status(400).send(error)
+    // })
+})
+
+app.get('/tasks/:id', async (req,res)=>{
+    const _id=req.params.id
+    try{
+        const task=await Task.findById(_id);
+        if(!task){
+            res.status(404).send("not found")
+        }
+        res.send(task);
+    }
+    catch(e){
+        res.status(500).send()
+    }
+    // Task.findById(_id).then(task=>{
+    //     if(!task){
+    //         res.status(400).send("not found")
+    //     }
+    //     res.send(task)
+    // }).catch(error=>{
+    //     res.status(400).send(error)
+    // })
+})
+
+app.patch('/tasks/:id',async (req,res)=>{
+    const id=req.params.id;
+    const updates=Object.keys(req.body);
+    const allowedupdates=['description','completed']
+    const isValid = updates.every(update=>allowedupdates.includes(update));
+
+    if(!isValid){
+        return res.status(400).send("not valid operation") 
+    }
+    try{
+        //new ensures that updated result is returned, runvlaidators ensure that validation occurs else in this case empty name will be saved
+        const user= await Task.findByIdAndUpdate(id,req.body,{new:true,runValidators:true});
+        if(!user){
+            res.status(400).send()
+        }
+        res.send(user)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+})
+app.delete('/tasks/:id',async(req,res)=>{
+    try{
+        const task= await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            res.status(400).send("not found task")
+        }
+        res.send(task);
+    }catch(e){
+        res.status(500).send(e);
+    }
 })
 
 app.listen(port,()=>{
