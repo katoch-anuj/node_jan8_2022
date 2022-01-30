8bbea6276ae86043a10e5710a2ff217d71f17f33
anuj.katoch <anuj.katoch@timesinternet.in>
2022-01-30 21:48:59 +0530

    clearing code for patch and deleted based upon id to token based

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -33,63 +33,6 @@ router.get("/users/me", auth, async (req,res)=>{
     
 })
 
-// not required as above takes care
-// router.get("/users/:id", async (req,res)=>{
-//     const _id=req.params.id;
-//     try{
-//         const user=await User.findById(_id);
-//         if(!user){
-//             res.status(404).send("not found")
-//         }
-//         res.send(user);
-//     }
-//     catch(e){
-//         res.status(500).send()
-//     }
-//     // User.findById(_id).then(user=>{
-//     //     //if we dont get nay result, for mongo db that's success cz mongo db was able to search what u asked and came up with no result.
-//     //     if(!user){
-//     //         res.status(404).send("not found")
-//     //     }
-//     //     res.send(user);
-//     // }).catch(error=>{
-//     //     res.status(500).send()
-//     // })
-// })
-/**
- *  Updating
- */
-
-// router.patch('/users/:id',async (req,res)=>{
-//     const id=req.params.id;
-//     const updates=Object.keys(req.body);
-//     const allowedupdates=['name','age','email','password']
-//     const isValid = updates.every(update=>allowedupdates.includes(update));
-
-//     if(!isValid){
-//         return res.status(400).send("not valid operation") 
-//     }
-//     try{
-//         //new ensures that updated result is returned, runvlaidators ensure that validation occurs else in this case empty name will be saved
-//         //this will bypass the middleware for schema so updating this.
-//         // const user= await User.findByIdAndUpdate(id,req.body,{new:true,runValidators:true});
-//        //below code is done so that our middleware runs and we dont update db directly
-//         const user = await User.findById(id);
-//         updates.forEach(update=>{
-//             user[update]=req.body[update];
-//         });
-//         await user.save();
-//         //new code neds here
-
-//         if(!user){
-//             res.status(400).send()
-//         }
-//         res.send(user)
-//     }
-//     catch(e){
-//         res.status(500).send(e)
-//     }
-// })
 router.patch('/users/me', auth,async (req,res)=>{
     const updates=Object.keys(req.body);
     const allowedupdates=['name','age','email','password']
@@ -108,18 +51,6 @@ router.patch('/users/me', auth,async (req,res)=>{
     }
     
 })
-//updating the delete route
-// router.delete('/users/:id',async(req,res)=>{
-//     try{
-//         const user= await User.findByIdAndDelete(req.params.id)
-//         if(!user){
-//             res.status(400).send("not found user")
-//         }
-//         res.send(user);
-//     }catch(e){
-//         res.status(500).send(e)
-//     }
-// })
 router.delete('/users/me',auth,async(req,res)=>{
     try{
         await req.user.remove()
